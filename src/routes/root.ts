71b52f11875de901f092f5d279d7705277ff585c
ParticleG <particle_g@outlook.com>
2023-09-22 17:51:46 +0800

    - Update root route

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,8 +1,8 @@
 import { FastifyPluginAsync } from 'fastify';
 
-const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true };
+const root: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.get('/', async function () {
+    return { time: new Date().toLocaleString(), root: true };
   });
 };
 
