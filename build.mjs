1991e5ffcdaed39fca022b4bb824a00a9968008e
ParticleG <particle_g@outlook.com>
2023-09-25 10:16:58 +0800

    - Update sources

--- build.mjs
+++ build.mjs
@@ -1,6 +1,10 @@
-import * as esbuild from 'esbuild';
-import { createBuildSettings } from './setting.mjs';
+import * as esbuild from "esbuild";
+import { rm } from "node:fs/promises";
+
+import { createBuildSettings } from "./setting.mjs";
 
 const settings = await createBuildSettings({ minify: true });
+
+await rm(settings.outdir, { recursive: true, force: true });
 
 await esbuild.build(settings);
