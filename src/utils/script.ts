c0c285ed727e3a202bec0e3e2ff6d5c3ecd874cd
ParticleG <particle_g@outlook.com>
2023-11-03 17:48:55 +0800

    - Bump version

--- src/utils/script.ts
+++ src/utils/script.ts
@@ -1,17 +1,21 @@
 import { databaseManager } from 'components/DatabaseManager';
-import { decode } from 'iconv-lite';
 import { readFileSync } from 'fs';
 import { runVbs } from '@el3um4s/run-vbs';
+import * as console from 'console';
 
 export const loginPrompt = async () => {
-  const script = decode(
-    readFileSync('./src/assets/login.vbs'),
-    'gb2312',
-  ).toString();
+  // const script = decode(
+  //   readFileSync('./src/assets/login.vbs'),
+  //   'gb2312',
+  // ).toString();
+  const script = readFileSync('./src/assets/login.vbs').toString();
+  console.log(script);
   let isSuccess = false;
   do {
     await databaseManager.authCode();
-    const { success } = JSON.parse(await runVbs({ vbs: script }));
+    const result = await runVbs({ vbs: script });
+    console.log(result);
+    const { success } = JSON.parse(result);
     isSuccess = success;
   } while (!isSuccess);
 };
