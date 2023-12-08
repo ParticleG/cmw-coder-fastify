3b6e9d2d71ab21c1d712e46dd50a97b6bd50c8bf
CHFKA <953290202@qq.com>
2023-12-08 15:54:58 +0800

    增加黄区上报不同产品线的模型

--- src/plugins/statistics.ts
+++ src/plugins/statistics.ts
@@ -18,6 +18,13 @@ const secondClassMap = new Map<HuggingFaceModelType | LinseerModelType, string>(
     [LinseerModelType.Linseer_CClsw, 'LS13B'],
   ],
 );
+const productLineMap = new Map<LinseerModelType, string >(
+  [
+    [LinseerModelType.Linseer, "H3C 通用"],
+    [LinseerModelType.Linseer_CClsw, "交换机产品线"],
+    [LinseerModelType.Linseer_SR88Driver, ""], //高端路由模型还未推出，产品线字段暂时返回空，后续更新删除该注释
+  ]
+);
 
 const constructData = (
   completion: string,
@@ -42,6 +49,9 @@ const constructData = (
     user: username,
     userType: 'USER',
   };
+  if (modelType as LinseerModelType) {
+    Object.assign(basicData, {productline: productLineMap.get(modelType)})
+  }
 
   return [
     {
